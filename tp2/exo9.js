2bb7f192ed77f9f57ba4769d6271ee9ca7590731
yuri guemra <yuri.guemra@live.fr>
2022-12-21 10:52:33 +0100

    cour et tp

--- tp2/exo9.js
+++ tp2/exo9.js
@@ -1,1 +1,8 @@
-//Écrivez un programme Node.js qui utilise le module querystring pour analyser une chaîne de requête URL.
+//Écrivez un programme Node.js qui utilise le module querystring pour analyser une chaîne de requête URL.const querystring = require('querystring');
+const querystring = require('querystring');
+
+const url = ('https://nodejs.org/dist/latest-v19.x/docs/api/querystring.html');
+
+console.log(querystring.parse(url));
+
+
