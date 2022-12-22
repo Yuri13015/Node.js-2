a0685d168f1134b83615df1cb215c29538b1d8ee
yuri guemra <yuri.guemra@live.fr>
2022-12-22 14:46:54 +0100

    cour et tp

--- tp fianl/tpFinal.js
+++ tp fianl/tpFinal.js
@@ -1,9 +1,36 @@
-console.log('Bienvenue dans notre quizz!!!');
-const readline = require('readline');
-const name = readline.createInterface(process.stdin,process.stdout);
-const username = name.question('Entrer votre prénom  ',(reponse)=>{
+const cells = document.querySelectorAll('[data-cell]');
+const gameStatus = document.getElementById('gameStatus');
+const endGameStatus = document.getElementById('endGameStatus');
+const playerOne = 'X'; const playerTwo = 'O';
+let playerTurn = playerOne;
 
-    console.log('bonjour '+reponse+' bon courage tu en auras besoin')
+const winningPatterns = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
 
+cells.forEach(cell => {
+  cell.addEventListener('click', playGame, { once: true });
 });
+
+function playGame(e) {
+  e.target.innerHTML = playerTurn;
+
+  if (checkWin(playerTurn)) {
+    updateGameStatus("wins" + playerTurn);
+    return endGame();
+  } else if (checkDraw()) {
+    updateGameStatus("draw");
+    return endGame();
+  }
+
+  updateGameStatus(playerTurn);
+  playerTurn == playerOne ? playerTurn = playerTwo : playerTurn = playerOne;
+}
 
